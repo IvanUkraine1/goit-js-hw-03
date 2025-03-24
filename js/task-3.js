c1353091bed505cc75c234e625dab27d9915d00b
NeLyudyuna00 <118217807+IvanUkraine1@users.noreply.github.com>
2025-03-24 02:36:15 +0200

    second git

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 function filterArray(numbers, value){
     let resultArray = [];
-    for(let i = 0; i <= numbers.length; i++){
+    for(let i = 0; i < numbers.length; i++){
         if(numbers[i] > value){
             resultArray.push(numbers[i]);
         }
